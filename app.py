58613f448becd99030fe261fce27714c2ce08c51
danfiliphoff <danfiliphoff@hotmail.com>
2020-01-06 09:56:32 +0000

    '' added around mongo URI

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'books'
 #förösk spara danhoff:UrSEzkJwRg0yELxb i environmental variabel så att lösenord ej syns på githu
-app.config["MONGO_URI"] = mongodb+srv://danhoff:UrSEzkJwRg0yELxb@bookscluster-pabzd.mongodb.net/books?retryWrites=true&w=majority
+app.config["MONGO_URI"] = 'mongodb+srv://danhoff:UrSEzkJwRg0yELxb@bookscluster-pabzd.mongodb.net/books?retryWrites=true&w=majority'
 
 
 mongo = PyMongo(app)
