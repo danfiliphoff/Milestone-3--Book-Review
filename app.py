c2a98fc293b6d5de684a3b80bf81cfff22b65e35
danfiliphoff <danfiliphoff@hotmail.com>
2020-01-06 08:36:09 +0000

    test to see if database is correctly conected

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-#test coment
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'books'
 app.config["MONGO_URI"] = 'mongodb+srv://danhoff:jagersnart21@bookscluster-pabzd.mongodb.net/books?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_books')
 def get_books():
     return render_template("books.html", books=mongo.db.books.find())
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
