196eb2052ed993abc8ff70ba5fb54a6edf93feaa
danfiliphoff <danfiliphoff@hotmail.com>
2020-01-11 05:33:30 +0000

    basics for adding a review in place

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ app.config["MONGO_DBNAME"] = 'books'
 #förösk spara danhoff:UrSEzkJwRg0yELxb i environmental variabel så att lösenord ej syns på gith
 app.config["MONGO_URI"] = 'mongodb+srv://danhoff:UrSEzkJwRg0yELxb@bookscluster-pabzd.mongodb.net/books?retryWrites=true&w=majority'
 
-
 mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_books')
 def get_books():
     return render_template("books.html", books=mongo.db.books.find())
+
+@app.route('/add_book')
+def add_book():
+    return render_template("addbook.html")
+
 
 
 if __name__ == '__main__':
