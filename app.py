78e7e3fae39889ef7e88c51ce0a6cb893f6afe3a
danfiliphoff <danfiliphoff@hotmail.com>
2020-02-19 02:50:07 +0000

    vote update adjusted

--- app.py
+++ app.py
@@ -60,8 +60,7 @@ def upvote_book(book_id):
            # add one to "votes"
     votes = book['votes'] + 1 
         # submit new total value of "votes" to mongo db 
-    book.update( {'_id': ObjectId(book_id)},
-    {
+    book.update({
         'votes':request.form.get('votes'),
     })
         
