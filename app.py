dc63b07f2dda0969eac8baca6be4d42b4d51190f
danfiliphoff <danfiliphoff@hotmail.com>
2020-02-24 06:12:11 +0000

    votes update changed

--- app.py
+++ app.py
@@ -60,8 +60,9 @@ def upvote_book(book_id):
     book = mongo.db.books.find_one({'_id': ObjectId(book_id)})
     votes = book['votes'] + 1
     # submit new total value of "votes" t mongo db 
-    db.books.update(
-        {_id: book_id, $set: {"votes": votes}}
+    mongo.db.books.update(
+        {"_id": book_id},
+        {"$set": {"votes": votes}}
     )
     book = mongo.db.books.find_one({'_id': ObjectId(book_id)})
     print(f'modified: {book}')
