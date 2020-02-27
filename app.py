6d868cf4a4b052bbf5e08ac13048d2d353422568
danfiliphoff <danfiliphoff@hotmail.com>
2020-02-27 00:36:02 +0000

    adjustement to upvote function

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def delete_book(book_id):
 #below code to count votes
 @app.route('/upvote_book/<book_id>')
 def upvote_book(book_id):
-    book = mongo.db.books.update_one({'_id': ObjectId(book_id)})
+    book = mongo.db.books.find_one({'_id': ObjectId(book_id)})
     votes = book['votes'] + 1
     # submit new total value of "votes" t mongo db 
     mongo.db.books.update_one(
