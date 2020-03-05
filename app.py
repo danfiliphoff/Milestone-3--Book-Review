4b346fc085683a068b7e3c6c7bbc55053a1ca0ec
danfiliphoff <danfiliphoff@hotmail.com>
2020-03-05 05:10:39 +0000

    function for genre select comented out

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ def genres():
     return render_template("genre.html",
         genre=mongo.db.genre.find().sort("genre_name"))
 
+#on selecting a genre
+##@app.route('/selected_genre')
+#argumente ska baseras på vad man har klickat på
+d#ef selected_genre(genre_name):
+    #return render_template("genre.html", books=mongo.db.books.find().sort("votes", -1))
+
+
 @app.route('/insert_book', methods=['POST'])
 def insert_book():
     books = mongo.db.books
